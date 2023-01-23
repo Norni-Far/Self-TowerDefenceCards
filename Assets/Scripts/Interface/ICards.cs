3ae5736e88bd0ce0d1bf8401b81e5fcccc4532ae
Norni-far <the.somnium.project.games@gmail.com>
2023-01-23 23:43:54 +0300

    default interfaces

---
+++ Assets/Scripts/Interface/ICards.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICards
+{
+    public int IdCards { get; set; }
+
+
+
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public void SpawnObject(IEnemy enmey, Transform positionOfSpawn)
+    {
+
+
+
+
+    }
+
+    public void SpawnObject(ITower tower, Transform positionOfSpawn)
+    {
+
+
+
+
+    }
+
+}
